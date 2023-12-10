e71a60e27bc9a0e5ca4140ed826661e9da7cdee9
rkirgizov <r.kirgizov@live.com>
2023-12-10 20:34:08 +0300

    1

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+# импорты
+from config_reader import config
+
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
-from config import TOKEN
-bot = Bot(token=TOKEN)
+# Для записей с типом Secret* необходимо 
+# вызывать метод get_secret_value(), 
+# чтобы получить настоящее содержимое вместо '*******'
+bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def process_start_command(message: types.Message):
-        await message.reply("Привет!\nНапиши мне что-нибудь!")
+        await message.answer("Привет!\nНапиши мне что-нибудь!")
     
 @dp.message(Command("help"))
 async def process_help_command(message: types.Message):
